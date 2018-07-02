f6e8c35452a0c0b8288a3a4fe2e4eb11eec28018
dibashi <dibashi1989@163.com>
2018-07-02 17:11:12 +0800

    添加一些供调试的资源

--- MyBalloon/assets/Script/game.js
+++ MyBalloon/assets/Script/game.js
@@ -13,9 +13,8 @@ cc.Class({
        //初始化要根据checkpoint来读取相应的关卡数据
        
        //1读取 数据 还是加载场景？ wtf
-       cc.log("game scenen!");
-       //读取prefab 然后放入到 gameLayer中，让 卫士，气球，敌人，奖励品，墙之间去交互
-       //这个prefab中 会有敌人和奖品（金币）还会有一些障碍物（黑色的墙）
+
+       
     },
 
     // called every frame
