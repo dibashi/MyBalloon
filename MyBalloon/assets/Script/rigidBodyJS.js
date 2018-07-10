3f82ddbb42d529e49b6f8aca994db7a2a69a1685
dibashi <dibashi1989@163.com>
2018-07-10 13:32:01 +0800

    第九关 bug fix

--- MyBalloon/assets/Script/rigidBodyJS.js
+++ MyBalloon/assets/Script/rigidBodyJS.js
@@ -50,7 +50,7 @@ cc.Class({
         if (this.gravityHasBeenGiven == false) { //没给过重力
             if (this.gravityFlagOfHit == true) { //根据阀值给重力
 
-                //cc.log("碰撞： 给到重力！");
+                cc.log("碰撞： 给到重力！");
                 this.node.getComponent(cc.RigidBody).gravityScale = 1;
                 this.gravityHasBeenGiven = true;
             }
