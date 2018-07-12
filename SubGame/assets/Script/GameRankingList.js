f42b71899952afe7f6dd35dca6a6fc1af0b70ad2
dibashi <dibashi1989@163.com>
2018-07-12 20:06:48 +0800

    完善超越

--- SubGame/assets/Script/GameRankingList.js
+++ SubGame/assets/Script/GameRankingList.js
@@ -9,12 +9,149 @@ cc.Class({
         gameOverRankLayout: cc.Node,
         loadingLabel: cc.Node,//加载文字
 
-        beyondFriendNode:cc.Node,
+        beyondFriendNode: cc.Node,
+
+
+
+        headImageNode: cc.Node,
+        waitingForBeyondFriends: null,
     },
+
+
+
+    isBeyond: function (currentScore) {
+        if (this.waitingForBeyondFriends == null || this.waitingForBeyondFriends.length == 0) {
+            return;
+        }
+        console.log(this.waitingForBeyondFriends.length);
+        console.log("看下待超越的数据组");
+        for (let j = 0; j < this.waitingForBeyondFriends.length; j++) {
+            console.log(this.waitingForBeyondFriends[j]);
+        }
+
+
+        let beyondIndex = -1;
+        for (let i = this.waitingForBeyondFriends.length - 1; i >= 0; i--) {//这个数据源是已经排好序的，但是是倒序从大到小
+            let otherScore = this.waitingForBeyondFriends[i].KVDataList.length != 0 ? this.waitingForBeyondFriends[i].KVDataList[0].value : 0;
+            if (currentScore > otherScore) {
+                beyondIndex = i;
+                break;
+            }
+        }
+        if(beyondIndex != -1) {
+            this.beyondFriendNode.active = true;
+            //splice 返回的是一个数组。一定要加索引来访问
+            let beyondData = this.waitingForBeyondFriends.splice(beyondIndex, 1);
+            console.log("看下超越的玩家数据");
+            console.log(beyondData[0]);
+    
+            this.initSprite(beyondData[0]);
+        }
+       
+    },
+
+    initSprite: function (beyondData) {
+        let avatarUrl = beyondData.avatarUrl;
+         console.log("看下 头像 URL");
+         console.log(avatarUrl);
+         console.log(this.headImageNode);
+         console.log(this.headImageNode.getComponent(cc.Animation));
+        this.createImage(avatarUrl);
+         let anim = this.headImageNode.getComponent(cc.Animation);
+         anim.play();
+       // this.headImageNode.runAction(cc.moveTo(5.0,cc.v2(200,200)));
+    },
+
+    createImage(avatarUrl) {
+        try {
+            let image = wx.createImage();
+            image.onload = () => {
+                try {
+                    let texture = new cc.Texture2D();
+                    texture.initWithElement(image);
+                    texture.handleLoadedTexture();
+                    this.headImageNode.getComponent(cc.Sprite).spriteFrame = new cc.SpriteFrame(texture);
+                } catch (e) {
+                    cc.log(e);
+                    this.headImageNode.active = false;
+                }
+            };
+            image.src = avatarUrl;
+        } catch (e) {
+            cc.log(e);
+            this.headImageNode.active = false;
+        }
+    },
+
+    fetchFriendDataToBeyond(MAIN_MENU_NUM) {
+        this.node.active = true;
+        if (CC_WECHATGAME) {
+            wx.getUserInfo({
+                openIdList: ['selfOpenId'],
+                success: (userRes) => {
+                    console.log('超越部分：success', userRes.data)
+                    let userData = userRes.data[0];
+                    //取出所有好友数据
+                    wx.getFriendCloudStorage({
+                        keyList: [MAIN_MENU_NUM],
+                        success: res => {
+                            console.log("超越部分：wx.getFriendCloudStorage success", res);
+                            let data = res.data;
+                            data.sort((a, b) => {
+                                if (a.KVDataList.length == 0 && b.KVDataList.length == 0) {
+                                    return 0;
+                                }
+                                if (a.KVDataList.length == 0) {
+                                    return 1;
+                                }
+                                if (b.KVDataList.length == 0) {
+                                    return -1;
+                                }
+                                return b.KVDataList[0].value - a.KVDataList[0].value;
+                            });
+                            let waitingForDelete = 0;
+                            for (let i = 0; i < data.length; i++) {
+                                if (data[i].avatarUrl == userData.avatarUrl) {//这是自己，要从待超集合中删除
+                                    waitingForDelete = i;
+                                }
+                            }
+
+                            data.splice(waitingForDelete, 1);
+                            this.waitingForBeyondFriends = data;
+                            console.log("这里 这里！");
+                            for (let j = 0; j < this.waitingForBeyondFriends.length; j++) {
+                                console.log(this.waitingForBeyondFriends[j]);
+                            }
+
+                        },
+                        fail: res => {
+                            console.log("wx.getFriendCloudStorage fail", res);
+                            this.loadingLabel.getComponent(cc.Label).string = "数据加载失败，请检测网络，谢谢。";
+                        },
+                    });
+                },
+                fail: (res) => {
+                    this.loadingLabel.getComponent(cc.Label).string = "数据加载失败，请检测网络，谢谢。";
+                }
+            });
+        }
+    },
+
+
+
+
+
+
+
+
+
+
+
+
 
     start() {
         console.log("运行到 排行list！！！！");
-       // this.beyondFriendNode.active = false;
+
         this.removeChild();
         if (CC_WECHATGAME) {
             window.wx.onMessage(data => {
@@ -29,7 +166,14 @@ cc.Class({
                     this.gameOverRank(data.MAIN_MENU_NUM);
                 } else if (data.messageType == 5) {//获取群排行榜
                     this.fetchGroupFriendData(data.MAIN_MENU_NUM, data.shareTicket);
-                } 
+                } else if (data.messageType == 6) {//用于游戏内的超越功能的数据源获取
+                    this.removeChild();
+                    this.loadingLabel.active = false;
+                    // this.beyondFriendNode.active = true;
+                    this.fetchFriendDataToBeyond(data.MAIN_MENU_NUM);
+                } else if (data.messageType == 7) { //用于查询给的分数是否超过当前数据源中的分数，超过谁就显示谁，然后删除掉
+                    this.isBeyond(data.currentScore);
+                }
             });
         } else {
             this.fetchFriendData(1000);
@@ -81,6 +225,8 @@ cc.Class({
         this.gameOverRankLayout.removeAllChildren();
         this.loadingLabel.getComponent(cc.Label).string = "玩命加载中...";
         this.loadingLabel.active = true;
+
+        this.beyondFriendNode.active = false;
     },
     fetchFriendData(MAIN_MENU_NUM) {
         this.removeChild();
@@ -111,7 +257,7 @@ cc.Class({
                                 return b.KVDataList[0].value - a.KVDataList[0].value;
                             });
                             for (let i = 0; i < data.length; i++) {
-                                if(i<=8) {//先最多显示9条吧。以后再说
+                                if (i <= 8) {//先最多显示9条吧。以后再说
                                     var playerInfo = data[i];
                                     var item = cc.instantiate(this.prefabRankItem);
                                     item.getComponent('RankItem').init(i, playerInfo);
@@ -138,7 +284,7 @@ cc.Class({
         }
     },
 
-   
+
 
     fetchGroupFriendData(MAIN_MENU_NUM, shareTicket) {
         this.removeChild();
